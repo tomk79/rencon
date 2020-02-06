a7f1d10305e835e39c5df3732ba3ff91f601c022
Tomoya Koyanagi <tomk79@gmail.com>
2020-02-06 18:58:14 +0900

    <files> ファイルをオープンできるようになった

--- dist/rencon.php
+++ dist/rencon.php
@@ -2,7 +2,7 @@
 /* ---------------------
   rencon v0.0.1-alpha.1+dev
   (C)Tomoya Koyanagi
-  -- developers preview build @2020-02-06T09:22:52+00:00 --
+  -- developers preview build @2020-02-06T09:57:40+00:00 --
 --------------------- */
 
 // =-=-=-=-=-=-=-=-=-=-=-= Configuration START =-=-=-=-=-=-=-=-=-=-=-=
@@ -3239,6 +3239,101 @@ class rencon_apps_files_ctrl{
 		exit;
 	}
 
+	/**
+	 * ファイルを取得
+	 */
+	public function getfile(){
+		header('Content-type: text/json');
+		$path_root = $this->rencon->conf()->files_path_root;
+		$realpath_root = realpath($path_root);
+
+		$path_file = $_REQUEST['path_file'];
+		$value = array(
+			'result' => null,
+			'message' => null,
+			'ext' => null,
+			'mime' => null,
+			'base64' => null,
+		);
+
+		$realpath_file = realpath($realpath_root.'/'.$path_file);
+		if( $realpath_file === false ){
+			$value['result'] = false;
+			$value['message'] = 'File not found.'.$path_file;
+			echo json_encode($value);
+			exit;
+		}
+		if( !is_file($realpath_file) ){
+			$value['result'] = false;
+			$value['message'] = 'It is not a file.';
+			echo json_encode($value);
+			exit;
+		}
+		if( !is_readable($realpath_file) ){
+			$value['result'] = false;
+			$value['message'] = 'It is not readable.';
+			echo json_encode($value);
+			exit;
+		}
+
+		$value['base64'] = base64_encode( file_get_contents( $realpath_file ) );
+		if( preg_match( '/^.*\.([a-zA-Z0-9\_\-]*?)$/si', $realpath_file, $matched ) ){
+			$value['ext'] = strtolower($matched[1]);
+		}
+		$value['mime'] = $this->get_mime_type( $value['ext'] );
+		$value['result'] = true;
+		$value['message'] = 'OK';
+
+		header('Content-type: text/json');
+		echo json_encode($value);
+		exit;
+	}
+
+	/**
+	 * 拡張子から mime-type を得る
+	 */
+	private function get_mime_type($ext){
+		switch( $ext ){
+			case 'html':
+			case 'htm':
+				return 'text/html';
+				break;
+			case 'js':
+				return 'text/javascript';
+				break;
+			case 'css':
+			case 'scss':
+				return 'text/css';
+				break;
+			case 'gif':
+				return 'image/gif';
+				break;
+			case 'png':
+				return 'image/png';
+				break;
+			case 'jpg':
+			case 'jpeg':
+			case 'jpe':
+				return 'image/jpeg';
+				break;
+			case 'svg':
+				return 'image/svg+xml ';
+				break;
+			case 'text':
+			case 'txt':
+			case 'log':
+			case 'sh':
+			case 'bat':
+			case 'php':
+			case 'json':
+			case 'yml':
+			case 'yml':
+			case 'htaccess':
+				return 'text/plain';
+				break;
+		}
+		return false;
+	}
 }
 ?>
 <?php
@@ -3364,7 +3459,7 @@ if( !class_exists('PDO') ){
 <?php return ob_get_clean();
 }
 if( $app == 'databases' && $act == 'tables' ){
-ob_start(); ?><p>テーブル一覧です。</p>
+ob_start(); ?>
 <form action="?a=databases.tables&amp;dbkey=<?= htmlspecialchars( $this->rencon->req()->get_param('dbkey') ); ?>" method="post">
 <textarea name="db_sql" class="form-control"><?= htmlspecialchars( $this->rencon->req()->get_param('db_sql') ); ?></textarea>
 <input type="submit" value="クエリを実行" class="btn btn-primary" />
@@ -3477,24 +3572,38 @@ var remoteFinder = window.remoteFinder = new RemoteFinder(
 				},
 				body: dataBody
 			}).then(function (response) {
-				var contentType = response.headers.get('content-type').toLowerCase();
-				if(contentType.indexOf('application/json') === 0 || contentType.indexOf('text/json') === 0) {
-					response.json().then(function(json){
-						callback(json);
-					});
-				} else {
-					response.text().then(function(text){
-						callback(text);
-					});
-				}
+				response.json().then(function(json){
+					callback(json);
+				});
 			}).catch(function (response) {
-				// console.log(response);
+				console.error(response);
 				callback(response);
 			});
 		},
 		"open": function(fileinfo, callback){
-			alert('ファイル ' + fileinfo.path + ' を開こうとしています。この機能は開発中のため利用できません。');
-			callback(true);
+			console.log(fileinfo);
+			// alert('ファイル ' + fileinfo.path + ' を開こうとしています。');
+			var data = {
+				'path_file': fileinfo.path
+			};
+			var dataBody = Object.keys(data).map(function(key){ return key+"="+ encodeURIComponent(data[key]) }).join("&")
+			console.log(dataBody);
+			fetch("?a=files.getfile", {
+				method: "post",
+				headers: {
+					'content-type': 'application/x-www-form-urlencoded'
+				},
+				body: dataBody
+			}).then(function (response) {
+				response.json().then(function(json){
+					console.log(json);
+					window.open('data:'+json.mime+';base64,'+json.base64);
+					callback(true);
+				});
+			}).catch(function (response) {
+				console.error(response);
+				callback(false);
+			});
 		},
 		"mkdir": function(current_dir, callback){
 			var foldername = prompt('Folder name:');
@@ -3536,7 +3645,7 @@ var remoteFinder = window.remoteFinder = new RemoteFinder(
 );
 // console.log(remoteFinder);
 <?php
-$current_dir = __DIR__;
+$current_dir = __DIR__.DIRECTORY_SEPARATOR;
 if( realpath($this->rencon->conf()->files_path_root) !== realpath('/') ){
 	$current_dir = '/';
 }
--- src/rencon/apps/files/ctrl.php
+++ src/rencon/apps/files/ctrl.php
@@ -54,5 +54,100 @@ class rencon_apps_files_ctrl{
 		exit;
 	}
 
+	/**
+	 * ファイルを取得
+	 */
+	public function getfile(){
+		header('Content-type: text/json');
+		$path_root = $this->rencon->conf()->files_path_root;
+		$realpath_root = realpath($path_root);
+
+		$path_file = $_REQUEST['path_file'];
+		$value = array(
+			'result' => null,
+			'message' => null,
+			'ext' => null,
+			'mime' => null,
+			'base64' => null,
+		);
+
+		$realpath_file = realpath($realpath_root.'/'.$path_file);
+		if( $realpath_file === false ){
+			$value['result'] = false;
+			$value['message'] = 'File not found.'.$path_file;
+			echo json_encode($value);
+			exit;
+		}
+		if( !is_file($realpath_file) ){
+			$value['result'] = false;
+			$value['message'] = 'It is not a file.';
+			echo json_encode($value);
+			exit;
+		}
+		if( !is_readable($realpath_file) ){
+			$value['result'] = false;
+			$value['message'] = 'It is not readable.';
+			echo json_encode($value);
+			exit;
+		}
+
+		$value['base64'] = base64_encode( file_get_contents( $realpath_file ) );
+		if( preg_match( '/^.*\.([a-zA-Z0-9\_\-]*?)$/si', $realpath_file, $matched ) ){
+			$value['ext'] = strtolower($matched[1]);
+		}
+		$value['mime'] = $this->get_mime_type( $value['ext'] );
+		$value['result'] = true;
+		$value['message'] = 'OK';
+
+		header('Content-type: text/json');
+		echo json_encode($value);
+		exit;
+	}
+
+	/**
+	 * 拡張子から mime-type を得る
+	 */
+	private function get_mime_type($ext){
+		switch( $ext ){
+			case 'html':
+			case 'htm':
+				return 'text/html';
+				break;
+			case 'js':
+				return 'text/javascript';
+				break;
+			case 'css':
+			case 'scss':
+				return 'text/css';
+				break;
+			case 'gif':
+				return 'image/gif';
+				break;
+			case 'png':
+				return 'image/png';
+				break;
+			case 'jpg':
+			case 'jpeg':
+			case 'jpe':
+				return 'image/jpeg';
+				break;
+			case 'svg':
+				return 'image/svg+xml ';
+				break;
+			case 'text':
+			case 'txt':
+			case 'log':
+			case 'sh':
+			case 'bat':
+			case 'php':
+			case 'json':
+			case 'yml':
+			case 'yml':
+			case 'htaccess':
+				return 'text/plain';
+				break;
+		}
+		return false;
+	}
 }
 ?>
